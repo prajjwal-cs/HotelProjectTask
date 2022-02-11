e6ecd2a7ddc61c7ed41728ad6da9891303369714
Prajjwal <prajjwal199275@gmail.com>
2022-02-11 22:45:58 +0530

    Create RegularCustomer class

---
+++ src/model/customer/RegularCustomer.java
@@ -0,0 +1,11 @@
+/* Created by IntelliJ IDEA.
+
+Author: Prajjwal Pachauri(cypher)
+Date: 11-02-2022
+Time: 22:45
+File: RegularCustomer.java */
+package model.customer;
+
+public class RegularCustomer {
+
+}
