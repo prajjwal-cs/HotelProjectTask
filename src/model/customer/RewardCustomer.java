66359d50f8d86fbe437e3c2706d6538827b4b4ac
Prajjwal <prajjwal199275@gmail.com>
2022-02-11 22:46:32 +0530

    Create RewardCustomer class

---
+++ src/model/customer/RewardCustomer.java
@@ -0,0 +1,10 @@
+/* Created by IntelliJ IDEA.
+
+Author: Prajjwal Pachauri(cypher)
+Date: 11-02-2022
+Time: 22:46
+File: RewardCustomer.java */
+package model.customer;
+
+public class RewardCustomer {
+}
