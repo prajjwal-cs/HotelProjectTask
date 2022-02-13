f445a720d35f1f15fa12ba12ed08367a75df68e7
Prajjwal <prajjwal199275@gmail.com>
2022-02-13 23:19:36 +0530

    Implemented main() method

--- src/model/main/Main.java
+++ src/model/main/Main.java
@@ -6,12 +6,21 @@ Time: 23:14
 File: Main.java */
 package model.main;
 
+import model.customer.Customer;
+import model.customer.RegularCustomer;
+import model.customer.RewardCustomer;
 import model.hotel.Hotel;
 import model.hotel.Radisson;
 
 public class Main {
     public static void main(String[] args) {
         Hotel hotel = new Radisson();
+
+        Customer customer1 = new RegularCustomer();
+        customer1.bookHotel(hotel);
+
+        Customer customer2 = new RewardCustomer();
+        customer2.bookHotel(hotel);
 
     }
 }
